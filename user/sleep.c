b3539a9a3389c56de09151df229d7903aa4ec440
Colin <qingyu.wang@aliyun.com>
2020-09-08 22:02:30 +0800

    feat: implement sleep
    (eazy)

---
+++ user/sleep.c
@@ -0,0 +1,21 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int i;
+
+  if(argc < 2 || argc > 2){
+    fprintf(2, "Usage: sleep time\n");
+    exit();
+  }
+
+  // convert ascii to int
+  i = atoi(argv[1]);
+
+  // do syscall
+  sleep(i);
+
+  exit();
+}
